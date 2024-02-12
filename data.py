6758642868950df1d3ec07f8cf7eb25ec9c3dc61
msuneth <msuneth@gmail.com>
2024-02-12 16:15:23 +0530

    initial commit

---
+++ data.py
@@ -0,0 +1,37 @@
+question_data = [
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Film",
+     "question": "Matt Damon played an astronaut stranded on an extraterrestrial planet in both of the "
+                 "movies Interstellar and The Martian.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Film",
+     "question": "Samuel L. Jackson had the words, &#039;Bad Motherf*cker&#039; in-scripted on his "
+                 "lightsaber during the filming of Star Wars.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "There are 6 legendary cards in &quot;Clash Royale&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science &amp; Nature",
+     "question": "An average human can go two weeks without water.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "There are 2 player roles in Trouble in Terrorist Town.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Mathematics",
+     "question": "An equilateral triangle always has every angle measuring 60&deg;.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "General Knowledge",
+     "question": "&quot;Ananas&quot; is mostly used as the word for Pineapple in other languages.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy",
+     "category": "Entertainment: Japanese Anime &amp; Manga",
+     "question": "In the &quot;Toaru Kagaku no Railgun&quot; anime,  espers can only reach a maximum of "
+                 "level 6 in their abilities.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "History",
+     "question": "In World War ll, Great Britain used inflatable tanks on the ports of Great Britain to "
+                 "divert Hitler away from NormandyD-day landing.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Animals",
+     "question": "Cats have whiskers under their legs.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}
+]
